b7e8ded14ae5c95e040a392a76993e5ebc575200
Beniamin Pawelec <beniamin.pawelec@vividgames.com>
2021-02-03 20:30:48 +0100

    zadanie 4

---
+++ zadanie4/Assets/Scripts/CameraTransformation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraTransformation : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 Offset;
+
+    void Start()
+    {
+        Offset = transform.position - Player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + Offset;
+    }
+}
---
+++ zadanie4/Assets/Scripts/RotateTriggerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class RotateTriggerController : MonoBehaviour
+{
+    private bool _isInTrigged = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+            _isInTrigged = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+            _isInTrigged = false;
+    }
+
+    void Update()
+    {
+        var transform = GetComponent<Transform>();
+        if (_isInTrigged)
+            transform.Rotate(new Vector3(0.5f, 0, 0.5f), 1f);
+    }
+}
