0639883e7922893d5d90a8dfae6e477cdfd26ccf
Beniamin Pawelec <beniamin.pawelec@vividgames.com>
2021-02-03 19:44:34 +0100

    zadanie 6

---
+++ zadanie6/Assets/Sctiprs/StartAnimationScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimationScript : MonoBehaviour
+{
+    public KeyCode KeyToAnimateStart;
+    public GameObject Player;
+    private Animator _animator;
+
+    private static readonly int IsMovingDown 
+        = Animator.StringToHash("IsMovingDown");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = Player.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          _animator.
+              SetBool(
+                  IsMovingDown,
+                  Input.GetKey(KeyToAnimateStart)
+                  );
+    }
+}
